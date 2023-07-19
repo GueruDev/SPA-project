4e652aec3c69696e199ac5c0c8825362fdb7b3ad
Ruben Contreras <SteelMods@hotmail.com>
2023-07-18 22:14:27 -0500

    Codding routing | card template

--- js/addCards.js
+++ js/addCards.js
@@ -2,16 +2,16 @@ class addCards {
   constructor() {
     this.container = document.querySelector(`div.container`);
     this.counter = 0;
+    this.data = [];
     this.urls = [];
     this.descriptions = [];
     this.images = [];
     this.fetchData();
   }
   add() {
-    if(!document.startViewTransition){
-        this.addIt();
-    }
-    else {
+    if (!document.startViewTransition) {
+      this.addIt();
+    } else {
       document.startViewTransition(() => this.addIt());
     }
   }
@@ -25,28 +25,46 @@ class addCards {
     cards[`card${this.counter}`] = new card3D(newElement); // ! bug on mobile
   }
 
-  htmlCardElement(){
-    let div = document.createElement('div');
-    div.innerHTML = `<a class='card' href='/${this.urls[this.counter]}' onclick='route()'><img href='/${this.urls[this.counter]}' class='card__miniature' src='${this.images[this.counter]}' /><p class='card__description'>${this.descriptions[this.counter]}</p></a>`;
+  htmlCardElement() {
+    let div = document.createElement("div");
+    div.innerHTML = `<a class='card' href='/${
+      this.urls[this.counter]
+    }' onclick='Spa.routing()'><img href='/${
+      this.urls[this.counter]
+    }' class='card__miniature' src='${
+      this.images[this.counter]
+    }' /><p class='card__description'>${
+      this.descriptions[this.counter]
+    }</p></a>`;
     let card = div.firstChild;
     return card;
   }
-  async fetchData(){
+  async fetchData() {
+    const cors = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "cef41ea26cmsh7308dd6ccd72652p19d01cjsnfe9fcbd97b9a",
+        "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
+      },
+    };
     try {
-      const response = await fetch('https://api.escuelajs.co/api/v1/users');
+      const url = 'https://free-to-play-games-database.p.rapidapi.com/api/filter?tag=3d.mmorpg.fantasy.pvp&platform=pc';
+      const response = await fetch(url, cors);
       const data = await response.json();
-      this.urls = data.map(obj => obj.name);
-      this.descriptions = data.map(obj => obj.email);
-      this.images = data.map(obj => obj.avatar);
-      
+      this.data = data;
+      this.urls = data.map((obj) => obj.id);
+      this.descriptions = data.map((obj) => obj.short_description);
+      this.images = data.map((obj) => obj.thumbnail);
+
       return;
     } catch (error) {
-      console.error('I cannot fetch the data');
+      console.error("I cannot fetch the data");
+      console.error(error);
       return [];
     }
   }
 }
 const addVideosButton = new addCards();
 
-  // .then(names => console.log(`Names of categories: ${names}`))
-  // .catch(error => console.error(`Error: ${error}`));
+// .then(names => console.log(`Names of categories: ${names}`))
+// .catch(error => console.error(`Error: ${error}`));
--- js/router.js
+++ js/router.js
@@ -1,7 +1,31 @@
-const route = (event) => {
-    event = event || window.event;
+// const route = (event) => {
+//   event = event || window.event;
+//   event.preventDefault();
+//   window.history.pushState({}, "", event.target.attributes["href"].value);
+// };
+// window.route = route;
+
+// ! Decirle Edwind que se ocupe del problema con redirecciones 301
+
+class route{
+  constructor(){
+  }
+
+  routing(event){
+    event = window.event || event;
     event.preventDefault();
     window.history.pushState({},'',event.target.attributes['href'].value);
+    window.routing = this.routing;
+    urlLocationHandler();
+  };
+  async urlLocationHandler(){
+    const location = window.location.pathname;
+    if(location.length == 0) location = `/`;
+  }
+
+  cardTemplate(){
+    
+  }
 }
 
-window.route = route;
+const Spa = new route();
